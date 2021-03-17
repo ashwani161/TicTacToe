7287d9aebca5351849c984e4387a0978cca562d7
ashwani161 <ashwanin65@gmail.com>
2021-03-17 12:40:40 +0530

    Added:Choose the letter X or O to play

--- src/com/tictactoe/TicTacToe.java
+++ src/com/tictactoe/TicTacToe.java
@@ -8,7 +8,7 @@ public class TicTacToe {
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe");
 		initializeBoard();
-		
+		chooseMove();
 	}
 
 	public static void initializeBoard() {
@@ -20,5 +20,23 @@ public class TicTacToe {
 		System.out.println("-+-+-");
 	}
 
-	
+	public static void chooseMove() {
+		System.out.println("Enter your move:");
+		Scanner sc = new Scanner(System.in);
+		String Move = sc.nextLine();
+		if (Move.equalsIgnoreCase("X") || Move.equalsIgnoreCase("O")) {
+			System.out.println("Enter row number:");
+			int row = sc.nextInt();
+			System.out.println("Enter column number:");
+			int col = sc.nextInt();
+			if (board[row - 1][col - 1] == 'X' || board[row - 1][col - 1] == 'O') {
+				System.out.println("The place is already is filled");
+				chooseMove();
+			} else
+				board[row - 1][col - 1] = Move.toUpperCase().toCharArray()[0];
+		} else {
+			System.out.println("Enter your valid move,either X or O");
+		}
+	}
+
 }
